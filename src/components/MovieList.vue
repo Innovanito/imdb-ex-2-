6f2b15b96e71ab002031363fdc84e206b4151e56
Innovanito <khg05311@naver.com>
2021-09-27 13:33:25 +0900

    ep25 done

--- src/components/MovieList.vue
+++ src/components/MovieList.vue
@@ -3,7 +3,7 @@
     <div 
       :class="{'no-result': !movies.length}"
       class="inner">
-      <div v-if="loading" class="spinner-border text-primary"></div>
+      <Loader v-if="loading" />
       <div 
         v-if="message"
         class="message">
@@ -23,10 +23,12 @@
 
 <script>
 import MovieItem from './MovieItem.vue'
+import Loader from '~/components/Loader'
 
 export default {
   components: {
-    MovieItem
+    MovieItem,
+    Loader
   },
   computed: {
     movies() {
